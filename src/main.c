51451c92282c9e98c092deba93af299b5b9ed44e
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-05-30 20:51:54 -0500

    feat: Add Tree Node data structure and functions
    This program is based on a tree data structure, so we need to be able to
    represent and manipulate trees.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct TreeNode {
+  float data;
+  int num_children;
+  struct TreeNode **children;
+};
+
+struct TreeNode *new_node(float data, int children_count) {
+  struct TreeNode *node = malloc(sizeof(struct TreeNode));
+  node->data = data;
+  node->children = malloc(sizeof(struct TreeNode *) * children_count);
+  node->num_children = children_count;
+  return node;
+}
+
+struct TreeNode *add_child(struct TreeNode *parent, struct TreeNode *child) {
+  parent->children = realloc(parent->children,
+      sizeof(struct TreeNode *) * (parent->num_children + 1));
+  parent->children[parent->num_children++] = child;
+
+  return child;
 void render_treemap() {
 }
 
